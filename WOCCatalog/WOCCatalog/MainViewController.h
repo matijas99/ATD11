b90ae3a9fa38a0b3ab1433f4cd0ee86483b0be5e
matijas99 <msmalcelj@realnetworks.com>
2015-11-15 22:53:39 +0100

    Playing with storyboards;

---
+++ WOCCatalog/WOCCatalog/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  WOCCatalog
+//
+//  Created by Matija Smalcelj on 15/11/15.
+//  Copyright © 2015 MSFT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
